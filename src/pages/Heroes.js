457f1df902c1b74da5f404cec4b3d94812bb21b0
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-03-29 15:08:19 -0700

    added HeroCard component and features

--- src/pages/Heroes.js
+++ src/pages/Heroes.js
@@ -1,22 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import HeroCard from '../components/HeroCard';
 import { heroData } from '../data/heroes.js';
 import { Link } from 'react-router-dom';
 
 const HeroesPage = () => {
-    const [heroes, setHeroes] = useState(heroData);
-    console.log(heroData);
+    const [heroes, setHeroes] = useState([]);
+    const [alert, setAlert] = useState(false);
+useEffect(() => {
+    setHeroes(heroData);
+
+}, [alert]);
+
+const updateFeatured = (heroId) => {
+    let foundHero = heroData.find(hero => hero.id === +heroId);
+    foundHero.featured = !foundHero.featured;
+    setAlert(true);
+    setTimeout(() => {
+    setAlert(false);
+    });
+    console.log(foundHero);
+};
+
+
     return (
         <div id='heroes'>
-            <ul>
-                {heroes.map((hero, index) => {
+            <div className='row text-center mt-3'>
+                <div className='col'>
+                    <h2>View Our Hero Database</h2>
+                </div>
+            </div>
+            <div className='row'>
+                {heroes.map((hero) => {
                     return (
-                    <li key={hero.id}>
-                        <Link to={`/heroes/${hero.id}`}>{hero.superhero}</Link>
-                       </li>
-                        );
+                        <div className='col-sm-12 col-md-3'>
+                            <HeroCard hero={hero} updateFeatured={updateFeatured}/>
+                            </div>
+                    )
                 })}
-            </ul>
-            <h2>This is my HeroesPage!</h2>
+            </div>
+           
+               
+            
         </div>
     );
 };
