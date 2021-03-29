c13192085797f8aebf4169cfde1d2a6b40035cc6
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-03-29 12:43:03 -0700

    added image css

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,3 +1,5 @@
+
+
 export const heroData = [
   {
       id: 1,
@@ -26,7 +28,7 @@ export const heroData = [
       alter_ego: 'Jay Garrick',
       first_appearance: 'Flash Comics #1',
       characters: ['Jay Garrick', 'Barry Allen', 'Wally West', 'Bart Allen'],
-      featured: false,
+      featured: true,
       image_url:'https://www.trendsinternational.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/5/15189---justice-league---the-flash.jpg',
     },
     {
@@ -45,7 +47,7 @@ export const heroData = [
         'Sinestro',
         'Simon Baz',
       ],
-      featured: false,
+      featured: true,
     image_url:'https://i.pinimg.com/originals/c4/cc/a9/c4cca9d4908c58c99db4a8953f04201d.jpg',
   },
     {
@@ -55,7 +57,7 @@ export const heroData = [
       alter_ego: 'Oliver Queen',
       first_appearance: 'More Fun Comics #73',
       characters: ['Oliver Queen'],
-      featured: false,
+      featured: true,
        image_url:'https://cdn.wallpapersafari.com/90/77/f0a1lr.jpg',
       },
     {
@@ -75,7 +77,7 @@ export const heroData = [
       alter_ego: "J'onn J'onzz",
       first_appearance: 'Detective Comics #225',
       characters: ['Martian Manhunter'],
-      featured: false,
+      featured: true,
       image_url:'https://cdn11.bigcommerce.com/s-b78xj/images/stencil/1280x1280/products/4363/18404/supergirl-martian-manhunter-deluxe-collectible-figure-star-ace-903911-07__98583.1533914764.jpg?c=2',
     },
     {
@@ -85,7 +87,7 @@ export const heroData = [
       alter_ego: 'Dick Grayson',
       first_appearance: 'Detective Comics #38',
       characters: ['Dick Grayson'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/564x/29/c9/53/29c953d035ca8a78bf82fe331e11ba00.jpg',
     },
     {
@@ -95,7 +97,7 @@ export const heroData = [
       alter_ego: 'Dan Garret',
       first_appearance: 'Mystery Men Comics #1',
       characters: ['Dan Garret', 'Ted Kord', 'Jaime Reyes'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/74/39/a5/7439a533be5255f72ce594c501d9d370.jpg'
     },
     {
@@ -105,7 +107,7 @@ export const heroData = [
       alter_ego: 'Dinah Drake',
       first_appearance: 'Flash Comics #86',
       characters: ['Dinah Drake', 'Dinah Lance'],
-      featured: false,
+      featured: true,
       image_url:'https://www.dccomics.com/sites/default/files/imce/2014/11-NOV/AR310A_BTS_1335ra.c_5463a5ff970e80.23545368.jpg'
     },
     {
@@ -125,7 +127,7 @@ export const heroData = [
       alter_ego: 'Steve Rogers',
       first_appearance: 'Captain America Comics #1',
       characters: ['Steve Rogers'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/a2/1d/17/a21d1753b96af3597d102a9748ef89eb.jpg',
     },
     {
@@ -145,7 +147,7 @@ export const heroData = [
       alter_ego: 'Thor Odinson',
       first_appearance: 'Journey into Myster #83',
       characters: ['Thor Odinson'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/7e/b9/64/7eb9649a7015eab34c233bae0ba3adba.jpg',
     },
     {
@@ -175,7 +177,7 @@ export const heroData = [
       alter_ego: 'Matthew Michael Murdock',
       first_appearance: 'Daredevil #1',
       characters: ['Matthew Michael Murdock'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/53/e8/55/53e855725e3f734ab77380e4e27c60a5.jpg',
     },
     {
@@ -185,7 +187,7 @@ export const heroData = [
       alter_ego: 'Clinton Francis Barton',
       first_appearance: 'Tales of Suspense #57',
       characters: ['Clinton Francis Barton'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/564x/e9/c8/ce/e9c8ce0f60038b8969d507ede3a0ff8f.jpg',
     },
     {
@@ -195,7 +197,7 @@ export const heroData = [
       alter_ego: 'Scott Summers',
       first_appearance: 'X-Men #1',
       characters: ['Scott Summers'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/fc/c1/68/fcc168575a3f99f31143004b16bcb656.jpg',
     },
     {
@@ -205,7 +207,7 @@ export const heroData = [
       alter_ego: 'Norrin Radd',
       first_appearance: 'The Fantastic Four #48',
       characters: ['Norrin Radd'],
-      featured: false,
+      featured: true,
       image_url:'https://i.pinimg.com/474x/c8/ff/ab/c8ffab5d52760f80ee36fe72432ca16d.jpg',
     },
 ]
